e059558ca24f464707b40de43bcbd32a269f54f1
raipen <raipen@knu.ac.kr>
2022-05-09 05:46:46 +0900

    rename html folder

--- src/routes/testTool.js
+++ src/routes/testTool.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const fs = require('fs');
 
 router.get('/',async (req, res) => {
-    fs.readFile('./src/html/test.html','utf-8',(error,data)=>{
+    fs.readFile('./src/global/test.html','utf-8',(error,data)=>{
         res.send(data);
     });
 });
